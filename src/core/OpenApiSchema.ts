73dfc023972aad2963a1b2707a81eddf0ef0232a
Stone Liu <liu.sto@northeastern.edu>
2025-04-29 19:55:09 -0400

    Constructor overloadding

--- src/core/OpenApiSchema.ts
+++ src/core/OpenApiSchema.ts
@@ -7,38 +7,30 @@ class OpenApiSchemaObject {}
 
 class OpenApiSchemaArray {}
 
-// Create a mapping type that links input types to output types
-type SchemaTypeMap<T extends OpenApiSchemaType> = T extends "string"
-  ? OpenApiSchemaPrimitive
-  : T extends "number"
-    ? OpenApiSchemaPrimitive
-    : T extends "boolean"
-      ? OpenApiSchemaPrimitive
-      : T extends "object"
-        ? OpenApiSchemaObject
-        : T extends "array"
-          ? OpenApiSchemaArray
-          : T extends "null"
-            ? OpenApiSchemaPrimitive
-            : never;
-
 export class OpenApiSchema {
   // Use the generic type parameter and the conditional mapping type
-  public static create<T extends OpenApiSchemaType>(type: T): SchemaTypeMap<T> {
+
+  public static create(type : "string") : OpenApiSchemaPrimitive
+  public static create(type : "number") : OpenApiSchemaPrimitive
+  public static create(type : "boolean") : OpenApiSchemaPrimitive
+  public static create(type : "null") : OpenApiSchemaPrimitive
+  public static create(type : "integer") : OpenApiSchemaPrimitive 
+  public static create(type : "object") : OpenApiSchemaObject 
+  public static create(type : "array") : OpenApiSchemaArray 
+  public static create(type: OpenApiSchemaType) {
     if (
       type === "string" ||
       type === "number" ||
       type === "boolean" ||
-      type === "boolean" ||
       type === "null"
     ) {
-      return new OpenApiSchemaPrimitive() as SchemaTypeMap<T>;
+      return new OpenApiSchemaPrimitive();
     }
     if (type === "object") {
-      return new OpenApiSchemaObject() as SchemaTypeMap<T>;
+      return new OpenApiSchemaObject();
     }
     if (type === "array") {
-      return new OpenApiSchemaArray() as SchemaTypeMap<T>;
+      return new OpenApiSchemaArray();
     }
     throw new Error("Not Found");
   }
