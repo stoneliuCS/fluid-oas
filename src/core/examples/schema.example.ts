ad7ad6567abeaccc2f6326db5546a0a00487fb8c
Stone Liu <liu.sto@northeastern.edu>
2025-05-10 00:41:17 -0400

    Insane

--- src/core/examples/schema.example.ts
+++ src/core/examples/schema.example.ts
@@ -1,4 +1,23 @@
 // Primitives
+import { OpenApiInteger } from "../schema/OpenApiInteger";
+import { OpenApiNumber } from "../schema/OpenApiNumber";
 import { OpenApiString } from "../schema/OpenApiString";
 
+OpenApiNumber.description("I am a OpenAPI Number!")
+  .format("double")
+  .min(0.5)
+  .max(2.5)
+  .exclusiveMin();
+
+OpenApiInteger.description("I am a OpenAPI Integer!")
+  .format("int64")
+  .min(0)
+  .max(99)
+  .exclusiveMax();
+
+OpenApiString.description("Unique identifier")
+  .format("uuid")
+  .pattern(
+    /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/,
+  );
 
