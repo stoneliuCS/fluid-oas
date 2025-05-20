353a92cc9feb78fffa48e556b8934a59fef94b90
Stone Liu <liu.sto@northeastern.edu>
2025-05-19 20:19:30 -0400

    Clean up schema types

--- src/core/schema/OpenApiSchema.ts
+++ src/core/schema/OpenApiSchema.ts
@@ -1,3 +1,6 @@
+import type { Boolean } from "./OpenApiBoolean";
+import type { Integer, Number } from "./OpenApiNumber";
+import type { Object } from "./OpenApiObject";
 import type { String } from "./OpenApiString";
 
-export type OpenApiSchema = String;
+export type OpenApiSchema = String | Number | Boolean | Integer | Object;
